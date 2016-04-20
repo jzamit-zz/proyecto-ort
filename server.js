d53918e9973ed482f7c3aaafed3bfab133ba7c03
Jorge Zamit <jzamit@gmail.com>
2016-04-20 09:49:03 -0300

    Cambios Puerto y URL de Heroku 3ra vez

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var passport = passport();  //Debo terminar la configuracion del Passport
 
 
 
-app.listen('https://vast-tundra-45875.herokuapp.com',443); //Puerto al que escucha
+app.listen(5005); //Puerto al que escucha
 module.exports=app;
 
-console.log('Servidor corriendo en http://vast-tundra-45875.herokuapp.com:443');
+console.log('Servidor corriendo en http://vast-tundra-45875.herokuapp.com:80');
