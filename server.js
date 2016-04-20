4a0af4f3e745ed4c8433ab56aa85c83a9e4d5480
Jorge Zamit <jzamit@gmail.com>
2016-04-20 09:55:29 -0300

    Prueba web a Heroku

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var passport = passport();  //Debo terminar la configuracion del Passport
 
 
 
-app.listen(5005); //Puerto al que escucha
+app.listen(process.env.PORT || 3000); //Puerto al que escucha
 module.exports=app;
 
 console.log('Servidor corriendo en http://vast-tundra-45875.herokuapp.com:80');
