0caf32e32edc58dcca5cea8df12a23c2cd4ad827
Jorge Zamit <jzamit@gmail.com>
2016-04-22 00:32:10 -0300

    Modifique Mongoose para que loguee conexion con Bd o Error si falla

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,7 +4,12 @@ var config = require('./config'),
 
 module.exports = function(){
 	
-var db = mongoose.connect(config.db);
+var db = mongoose.connect(config.db,function (error) {
+
+    if (error) console.error('Error conexion de Bd Mongo, Error: ' + error);
+    
+    else console.log('mongo connected');
+});
 
 require('../app/models/user.server.model');   // Estoy registrando el modelo user creado anteriormente
 
